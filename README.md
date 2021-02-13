50a8c9c4a9207fdf6adc4f90dce43ae323c3e8c1
Samuele Coassin <58360109+samuelecoassin@users.noreply.github.com>
2021-02-13 17:16:34 +0100

    Implementata possibilità di indicare diversi separatori. Aggiornato file README.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # StringsCalculator
 A simple calculator of integer numbers from a string
+
+Only positive integer numbers less than 1000 are accepted. 
+"," is the default separator, you can specify one or more different separators with the following format:
+//[delimiter]//
+For example:
+//[ABC][;]//15ABC15;30
+Returns 60
+
+Note: "[" and "]" are not valid as separator!
+
+Use Ctrl+C to exit the program.
+
--- StringsCalculator/Module1.vb
+++ StringsCalculator/Module1.vb
@@ -1,4 +1,6 @@
-﻿Module Module1
+﻿Imports System.Text.RegularExpressions
+
+Module Module1
 
     Sub Main()
         While True
@@ -10,9 +12,9 @@
                 Dim numero = Calcola(testo)
                 Console.WriteLine($"Totale: {numero}")
 
+
             Catch ex As Exception
-                Console.WriteLine($"Si è verificato un errore: {ex.Message}{vbNewLine}{ex.GetBaseException.Message}")
-                'Console.WriteLine("Si è verificato un errore. Verificare di aver rispettato il formato indicato.")
+                Console.WriteLine($"Si è verificato un errore: {ex.Message}")
             End Try
         End While
     End Sub
@@ -20,19 +22,69 @@
     Private Function Calcola(testo As String) As Integer
         Dim somma As Integer
         Dim numero As Integer
+        Dim numeriNegativi As New List(Of Integer)
 
         If String.IsNullOrEmpty(testo) Then Return 0
 
-        Dim parole = testo.Split(",")
+        Dim parole = SeparaTesto(testo)
+        If parole Is Nothing Then Return 0
 
-        ' Escludo le parole che non sono numeri validi e sommo il resto
+        ' Somma solo i numeri validi
         For Each parola In parole
             If Integer.TryParse(parola, numero) Then
-                somma += numero
+                If numero < 0 Then
+                    numeriNegativi.Add(numero)
+                ElseIf numero < 1000 Then
+                    somma += numero
+                End If
             End If
         Next
 
+        ' Se sono presenti numeri negativi, lancia un'eccezione
+        If numeriNegativi.Any() Then
+            Dim elencoNegativi = String.Join(", ", numeriNegativi)
+            Throw New Exception($"i numeri negativi non sono consentiti! Numeri indicati: {elencoNegativi}")
+        End If
+
         Return somma
+    End Function
+
+    Private Function SeparaTesto(testo As String) As String()
+        ' Verifica se sono richiesti delimitatori diversi
+        Dim elencoSeparatori As New List(Of String)
+        'TODO: implementare ricerca con le espressioni regolari per migliorare le performance e semplificare il codice
+        'Dim rgx = New Regex("^\/\/(\[(\w+)\])+\/\/")
+        'If rgx.IsMatch(testo) Then
+        '    For Each m In rgx.Matches(testo)
+
+        '    Next
+        'End If
+
+        If testo.StartsWith("//[") AndAlso testo.Contains("]//") Then
+            Dim separatori = testo.Substring(2, testo.IndexOf("]//") - 1)
+
+            For Each sep In separatori.Split("[")
+                If Not String.IsNullOrEmpty(sep) Then
+                    If Not sep.EndsWith("]") Then Throw New Exception("non è consentito utilizzare ""["" e ""]"" come separatori di numeri.")
+
+                    If sep.Length > 1 Then
+                        Dim sep2 = sep.Substring(0, sep.LastIndexOf("]"))
+                        If sep2.Contains("]") Then Throw New Exception("non è consentito utilizzare ""["" e ""]"" come separatori di numeri.")
+
+                        elencoSeparatori.Add(sep2)
+                    End If
+                End If
+            Next
+
+            testo = testo.Substring(testo.IndexOf("]//") + 3)
+            If String.IsNullOrEmpty(testo) Then Return Nothing
+        End If
+
+        If elencoSeparatori.Any() Then
+            Return testo.Split(elencoSeparatori.ToArray, StringSplitOptions.RemoveEmptyEntries)
+        Else
+            Return testo.Split(",")
+        End If
     End Function
 
 End Module
